bcef2860f3857b0626f2a90b4ec389f713e9b60e
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-16 18:33:27 -0600

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,54 @@ let createEmployeeRecords = function(employeeRowData) {
   })
 }
 
-let createTimeInEvent = function() {
-
+let createTimeInEvent = function(employeeRecord, timeStamp) {
+  let [date, hour] = timeStamp.split(" ")
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  })
+  return employeeRecord
 }
 
-let createTimeOutEvents = function(employeeRecord, timeStamp) {
-  
-  
+let createTimeOutEvent = function(employeeRecord, timeStamp) {
+  let [date, time] = timeStamp.split(" ")
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(time, 10),
+    date,
+  })
+  return employeeRecord
+}
+
+let hoursWorkedOnDate = function(employee, date) {
+  let inEvent = employee.timeInEvents.find(function(e){
+    return e.date === date
+})
+let outEvent = employee.timeOutEvents.find(function(e){
+    return e.date === date
+})
+return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(employee, dateSought) {
+  let rawWage = hoursWorkedOnDate(employee, dateSought)
+  * employee.payPerHour
+return parseFloat(rawWage.toString())
+}
+
+let allWagesFor = function(employee) {
+  let dates = employee.timeInEvents.map(e => e.date)
+  let wages = dates.map(d => wagesEarnedOnDate(employee, d))
+  return wages.reduce((a, b) => a+b )
+}
+
+let findEmployeeByFirstName = function(sourceArr, name) {
+  let employee = sourceArr.find(n => n.firstName === name)
+  return employee
+}
+
+let calculatePayroll = function(employees) {
+  let payroll = employees.map(e => allWagesFor(e))
+  return payroll.reduce((a, b) => a+b )
 }
