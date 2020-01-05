0e4a15e308ea88b08357c804c033c442f989e3b0
KristinHannah <kristinhannahk@gmail.com>
2020-01-05 16:17:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+
+function createEmployeeRecord(array) {
+    let employee = {
+        firstName: array[0], familyName: array[1], title: array[2], payPerHour: array[3], timeInEvents: [], timeOutEvents: []
+    };
+    return employee
+};
+
+function createEmployeeRecords(arrayOArrays) {
+    let objectArray = arrayOArrays.map(createEmployeeRecord)
+    return objectArray
+};
+
+function createTimeInEvent(obj, date) {
+    let record = obj
+    let dateTime = date.split(" ")
+    let monthNDay = dateTime[0]
+    let time = dateTime[1]
+    let timeIn = {
+        type: "TimeIn",
+        hour: parseInt(time),
+        date: monthNDay
+    }
+    record.timeInEvents.push(timeIn)
+    return record
+}
+
+function createTimeOutEvent(obj, date) {
+    let record = obj
+    let dateTime = date.split(" ")
+    let monthNDay = dateTime[0]
+    let time = dateTime[1]
+    let timeOut = {
+        type: "TimeOut",
+        hour: parseInt(time),
+        date: monthNDay
+    }
+    record.timeOutEvents.push(timeOut)
+
+    return record
+}
+
+function hoursWorkedOnDate(obj, date) {
+    let timeIn = obj.timeInEvents.find(element => element.date === date)
+    let timeOut = obj.timeOutEvents.find(element => element.date === date)
+    let hours = timeOut.hour - timeIn.hour
+    return hours / 100
+}
+
+function wagesEarnedOnDate(obj, date) {
+    let hours = hoursWorkedOnDate(obj, date)
+    let pay = hours * obj.payPerHour
+    return pay
+}
+
+function allWagesFor(obj) {
+    let allDates = obj.timeInEvents.map(element => element.date)
+    let allPay = 0
+    for (let i = 0; i < allDates.length; i++) {
+        allPay = allPay + wagesEarnedOnDate(obj, allDates[i])
+    }
+    return allPay
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    let eRecord = srcArray.find(element => element.firstName === firstName)
+    return eRecord
+}
+
+function calculatePayroll(array) {
+    let arrayOfWages = array.map(element => allWagesFor(element))
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    let wages = arrayOfWages.reduce(reducer)
+    return wages
+}
